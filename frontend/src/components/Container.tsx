7a4a19e0c697988bf8e828e9b2077fa5499e4993
marcosaraujo-dev <marcos.araso@hotmail.com>
2024-02-05 10:41:08 -0300

    Aula03

--- frontend/src/components/Container.tsx
+++ frontend/src/components/Container.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 export default function Container({ children }: { children: ReactNode }) {
-	return <div className="px-4 max-w-7xl mx-auto">{children}</div>;
+  return <div className="px-4 max-w-7xl mx-auto">{children}</div>;
 }
